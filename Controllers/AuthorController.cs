4256eda3cc6b24a1243519185e9137b5ed863325
Namne2506 <namne2506@gmail.com>
2024-10-08 13:18:54 +0700

    Add files via upload

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.LibraryManagement;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly Authorrepos repos;
+
+        public AuthorController(Authorrepos authorrepos)
+        {
+            repos = authorrepos;
+        }
+
+        [HttpGet]
+        public IActionResult Get(Author author)
+        {
+            try
+            {
+                var data = repos.Read(author);
+                return Ok(data);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        } 
+
+
+        [HttpPost]
+        public IActionResult Post(Author author)
+        {
+            try
+            {
+                var data = repos.Create(author);
+                return StatusCode(201, data);
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete]
+        public IActionResult Delete(Author author)
+        {
+            try
+            {
+                var data = repos.Delete(author);
+                return StatusCode(204, data);
+
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+    }
+}
